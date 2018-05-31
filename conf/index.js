67c6ddb67853b78002a8a5b61a0f1397c2109912
The Dickhead <ross.leitch@metamorphic.studio>
2018-06-01 00:19:57 +1200

    SSL

--- conf/index.js
+++ conf/index.js
@@ -2,5 +2,9 @@ module.exports = {
     mongo: {
         host: 'mongodb://localhost',
         db: 'wifidog-auth'
+    },
+    ssl: {
+        key: '/etc/letsencrypt/live/wifi.metamorphic.studio/privkey.pem',
+        cert: '/etc/letsencrypt/live/wifi.metamorphic.studio/cert.pem'
     }
 }
--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 var WifiDog = require('./lib/wifidog');
 var express = require('express');
+var http = require('http');
+var https = require('https');
 var app = express();
+var conf = require('./conf');
+var fs = require('fs');
+
+var key = fs.readFileSync(conf.ssl.key);
+var cert = fs.readFileSync(conf.ssl.cert);
 
 app.use(WifiDog);
-app.listen(80);
+
+https.createServer({
+    key: key,
+    cert: cert
+}, app).listen(443);
