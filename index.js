8b74838dfbf99e118aed2e9b163bd06b47c47e3f
The Dickhead <ross.leitch@metamorphic.studio>
2018-06-01 13:25:27 +1200

    Changed stuff

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var https = require('https');
 var app = express();
 var conf = require('./conf');
 var fs = require('fs');
-var session = require('express-session'):
+var session = require('express-session');
 
 var key = fs.readFileSync(conf.ssl.key);
 var cert = fs.readFileSync(conf.ssl.cert);
@@ -18,13 +18,7 @@ app.use(session({
 
 app.use(WifiDog);
 
-http.createServer();
-
-http.get('*', (req, res) => {
-    res.redirect('https://'+req.headers.host + req.url);
-});
-
-http.listen(80);
+http.createServer(app).listen(80);
 
 https.createServer({
     key: key,
